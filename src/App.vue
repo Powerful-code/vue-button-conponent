e3f1bcf592aea6d8215a48f6d903d5fce4433d4b
Powerful-code <kerimnurkaliev3@gmail.com>
2022-12-28 19:32:29 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+
+    <div class="wrapper">
+      <button-component :type="button" :link="true">Напомнить PIN-код</button-component>
+    </div>
+
+    <div class="wrapper">
+      <button-component color="primary" :icon="arrowLeft"></button-component>
+      <button-component color="danger" :icon="close"> </button-component>
+      <button-component color="warning" :icon="quest"> </button-component>
+      <button-component color="primary" :icon="arrowRight"> </button-component>
+      <button-component color="danger" :icon="googlePlus"> </button-component>
+      <button-component color="info" :icon="vk"> </button-component>
+      <button-component color="action" :icon="classmates"> </button-component>
+      <button-component color="primary" :icon="pencil"> </button-component>
+    </div>
+
+    <div class="wrapper">
+      <button-component :time="180">Repeat letter</button-component>
+    </div>
+
+    <div class="wrapper">
+      <button-component color="danger" type="button">send email</button-component>
+      <button-component color="primary" type="submit">send email </button-component>
+      <button-component color="secondary">send email</button-component>
+      <button-component color="action">send email</button-component>
+      <button-component color="warning">send email</button-component>
+      <button-component color="info">send email</button-component>
+      <button-component disabled="true">send email</button-component>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import ButtonComponent from "./components/Button/button.vue";
+import close from "./assets/icons/close.svg";
+import vk from "./assets/icons/vk.svg";
+import classmates from "./assets/icons/classmates.svg";
+import arrowLeft from "./assets/icons/arrow-left.svg";
+import arrowRight from "./assets/icons/arrow-right.svg";
+import pencil from "./assets/icons/pencil.svg";
+import quest from "./assets/icons/quest.svg";
+import googlePlus from "./assets/icons/google-plus.svg";
+
+export default {
+  components: {
+    ButtonComponent,
+  },
+  data() {
+    return {
+      close,
+      googlePlus,
+      quest,
+      pencil,
+      arrowLeft,
+      arrowRight,
+      classmates,
+      vk,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (max-width: 768px) {
+  .container {
+    flex-direction: column;
+    align-items: center;
+  }
+  .wrapper {
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ src/components/Button/button.vue
@@ -0,0 +1,166 @@
+<template>
+  <a href="#" v-if="link" class="link"><slot /></a>
+  <button
+    :class="['button', color, time && 'button--time', icon && 'button--icon']"
+    :disabled="disabled"
+    :type="type"
+    @click="countDown"
+    v-else
+  >
+    <img :src="icon" :alt="icon" v-if="icon" />
+    <slot v-else></slot>
+    <div class="timer" v-if="time">
+      {{ displayMinutes }}:{{ displaySeconds }}
+    </div>
+  </button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button-component",
+  props: {
+    disabled: Boolean,
+    color: String,
+    time: Number,
+    icon: String,
+    link: Boolean,
+    type: String,
+    content: String,
+  },
+  data() {
+    return {
+      counter: this.time,
+      displaySeconds: 0,
+      displayMinutes: 0,
+    };
+  },
+  mounted() {
+    (this.displayMinutes = Math.floor(this.counter / 60)),
+      (this.displaySeconds = this.counter % 60);
+    if (this.displaySeconds <= 9) {
+      this.displaySeconds = "0" + Math.floor(this.displaySeconds);
+    }
+    if (this.displayMinutes <= 9) {
+      this.displayMinutes = "0" + this.displayMinutes;
+    }
+  },
+  methods: {
+    countDown() {
+      if (this.counter && this.counter > 0) {
+        return setTimeout(() => {
+          this.displayMinutes = Math.floor(this.counter / 60);
+          this.displaySeconds = this.counter % 60;
+          if (this.displaySeconds <= 9) {
+            this.displaySeconds = "0" + Math.floor(this.displaySeconds);
+          }
+          if (this.displayMinutes <= 9) {
+            this.displayMinutes = "0" + this.displayMinutes;
+          }
+          --this.counter;
+          this.countDown();
+        }, 1000);
+      }
+    },
+  },
+});
+</script>
+
+<style setup>
+.button {
+  border-radius: 17px;
+  display: flex;
+  padding: 14px 46px;
+  margin-bottom: 20px;
+  font-family: Phosphate;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+  color: #ffffff;
+}
+
+.link {
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 18px;
+  transition: color 0.3s;
+  color: #fff;
+}
+.link:hover {
+  color: #767679;
+}
+.link:active {
+  color: #c4296c;
+}
+
+.button--icon {
+  width: 60px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  border-radius: 21px;
+}
+
+.button--time {
+  background: #efefef;
+  color: #767679;
+  font-family: "Phosphate";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+}
+
+.danger {
+  background-color: #df3f3e;
+}
+.primary {
+  background-color: #702c7e;
+}
+.secondary {
+  background-color: #c4296c;
+}
+.warning {
+  background-color: #f4ba46;
+}
+.disabled {
+  background-color: #efefef;
+}
+.info {
+  background-color: #0083b6;
+}
+.action {
+  background-color: #ed732e;
+}
+.button:disabled {
+  background-color: #efefef;
+  color: #767679;
+}
+
+.timer {
+  background-color: #df3f3e;
+  border-radius: 12px;
+  padding: 1px 6px 3px;
+  margin-left: 6px;
+  color: #fff;
+}
+
+@media screen and (max-width: 640px) {
+  .button--icon {
+    width: 52px;
+    height: 52px;
+  }
+  .button {
+    padding: 12px 26px;
+    font-size: 16px;
+  }
+}
+</style>
